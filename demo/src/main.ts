f17dca0fa42d9889596ab428cd3d1b3704c842aa
Andrew Bastin <andrewbastin.k@gmail.com>
2024-03-18 21:49:07 +0530

    chore: mark weird type error as a ts-expect-error

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -4,9 +4,10 @@ import App from './App.vue'
 import { plugin } from "vue-promise-modals"
 import { vCodePlugin } from "./plugins/v-code"
 
-createApp(App)
-.use(vCodePlugin)
-.use(plugin, {
+const app = createApp(App).use(vCodePlugin)
+ 
+// @ts-expect-error TODO: check weird type checking error here
+app.use(plugin, { 
   transition: {
     name: "modal" // Uses the modal-* classes for transition animations for the modals
   }
